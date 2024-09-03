f599b3fc056d78de46f55fdd75fa9b86da1fa4fc
GiorgiLomaia24 <admin@admins-MacBook-Pro.local>
2024-09-03 13:47:32 +0400

    sockets

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
     const { authuser } = useAuthContext();
     useEffect(() => {
         if (authuser) {
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://chat-app-prod-16vd.onrender.com", {
                 query: {
                     userId : authuser?._id
                 }
